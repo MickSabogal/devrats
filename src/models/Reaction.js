26c721e136000d4583edd466e886f2a3aa8592e1
msaheers <mishalsaheersha10@gmail.com>
2025-10-29 10:39:27 +0000

    minor updates on Reaction.js, all sorted now

--- src/models/Reaction.js
+++ src/models/Reaction.js
@@ -1,7 +1,7 @@
 const reactionSchema = new Schema({
   post: { type: Types.ObjectId, ref: 'Post', required: true },
   user: { type: Types.ObjectId, ref: 'User', required: true },
-  type: { type: String, enum: ['like','love','clap','laugh'], required: true }
+  type: { type: String, enum: ['like','love','clap','laugh', 'fire'], required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.models.Reaction || mongoose.model('Reaction', reactionSchema);
